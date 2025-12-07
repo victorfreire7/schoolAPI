fdb546fcb216f2b33d723a19c1b8d9d2c6b5b946
victorfreire7 <vhugo.freire159@gmail.com>
2025-12-07 19:13:18 -0300

    initial commit

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-export default () => {
-  console.log('Olá Mundo!');
+import express from 'express';
+import homeRoute from './src/routes/home.js'
+
+class App {
+  constructor() {
+    this.app = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+  }
+
+  routes() {
+    this.app.use('/', homeRoute)
+  }
 }
+
+export default new App().app;
