6dbe559142d73e950736522d6d66179cb614b22d
victorfreire7 <vhugo.freire159@gmail.com>
2025-12-07 19:14:02 -0300

    initial commit

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
-import falaOi from './app';
+import dotenv from 'dotenv';
+dotenv.config();
+import app from './app.js';
 
-falaOi();
+app.listen(parseInt(process.env.PORT), () => {
+  console.log(`http://localhost:${process.env.PORT}`);
+})
