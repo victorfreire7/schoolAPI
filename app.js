b498e373438aa29db4c9fd9d46c89443f432aec5
victorfreire7 <vhugo.freire159@gmail.com>
2025-12-07 20:35:14 -0300

    import database info

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from 'express';
-import homeRoute from './src/routes/home.js'
+import homeRoute from './src/routes/home.js';
+import './src/database'
+
 
 class App {
   constructor() {
