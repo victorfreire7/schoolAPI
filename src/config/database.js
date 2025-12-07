c224480cec314f467d363de235f70510c7a1a48c
victorfreire7 <vhugo.freire159@gmail.com>
2025-12-07 19:46:23 -0300

    initial commit

---
+++ src/config/database.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+module.exports = {
+  dialect: process.env.DB_DIALECT,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+    'createdAt': 'created_at', // para contrariar o erro comum no underscored do sequelize
+    'updatedAt': 'updated_at'
+  },
+  dialectOption: {
+    timezone: 'America/Sao_Paulo'
+  },
+  timezone: 'America/Sao_Paulo'
+}
