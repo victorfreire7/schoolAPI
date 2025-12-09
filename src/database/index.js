b4176d0a68f0948787ca350135e04723f69e6063
victorfreire7 <vhugo.freire159@gmail.com>
2025-12-08 22:25:20 -0300

    allow User model to DB connection

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,9 @@
 import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 import Aluno from '../models/Aluno';
+import User from '../models/User';
 
-const models = [Aluno];
+const models = [Aluno, User];
 const connection = new Sequelize(databaseConfig); // estou apenas me conectando, enviando as informaçoes utilizadas em outros arquivos que necessitam
 
 models.forEach((model) => model.init(connection));
