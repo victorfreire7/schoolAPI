a6fb03e4b8f9478ee7104c9974325573b4655a20
victorfreire7 <vhugo.freire159@gmail.com>
2025-12-08 22:24:41 -0300

    change mariadb to mysql dialect

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-  dialect: 'mariadb',
+  dialect: 'mysql',
   database: process.env.DB_NAME,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
