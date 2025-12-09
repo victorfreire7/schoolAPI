96510d805f67f0c1099b177781d995a8f3c6765c
victorfreire7 <vhugo.freire159@gmail.com>
2025-12-09 15:35:43 -0300

    create CRUD user

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -2,21 +2,91 @@ import User from '../models/User';
 
 class Controller {
   async index(req, res) {
-    res.json(
-      {
-        "index": true
-      }
-    )
+    try {
+      await User.findAll()
+      .then((users) => {
+        return res.json(users)
+      })
+    } catch {
+      return res.json(null);
+    }
   }
 
   async store(req, res) {
     try {
       await User.create(req.body)
-      .then((result) => {
-        res.json(result)
+      .then((user) => {
+        return res.json(user)
       })
     } catch (e) {
-      res.status(400).json(e.errors)
+      return res.status(400).json(e.errors);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      if(!req.params.id){
+        return res.status(400).json({
+          errors: ['missing id']
+        });
+      }
+
+      await User.findByPk(req.params.id)
+      .then((user) => {
+        if(!user){
+            return res.status(400).json({
+              errors: ['user not found']
+            });
+        } else {
+          res.json(user);
+        }
+      })
+    } catch (e) {
+      return res.status(400).json(e.errors);
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      await User.findByPk(req.params.id)
+      .then((user) => {
+        if(!user){
+            return res.status(400).json({
+              errors: ['user not found']
+            });
+        } else {
+          user.destroy();
+          res.status(200).json({
+            success: ['user delete successfuly']
+          });
+        }
+      })
+    } catch (e) {
+      return res.status(400).json(e.errors);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      if(!req.params.id){
+        return res.status(400).json({
+          errors: ['missing id']
+        });
+      }
+
+      await User.findByPk(req.params.id)
+      .then((user) => {
+        if(!user){
+            return res.status(400).json({
+              errors: ['user not found']
+            })
+        } else {
+          user.update(req.body);
+          res.json(user)
+        }
+      })
+    } catch (e) {
+      return res.status(400).json(e.errors);
     }
   }
 }
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,10 @@ import { Router } from 'express';
 import controller from '../controllers/User'
 const router = new Router();
 
-router.get('/', controller.index)
-router.post('/', controller.store)
+router.get('/', controller.index);
+router.post('/', controller.store);
+router.get('/:id', controller.show);
+router.put('/:id', controller.update);
+router.delete('/:id', controller.delete);
 
 export default router;
