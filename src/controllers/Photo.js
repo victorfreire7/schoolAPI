682a930714d9e1ca230827db7121069ed002e6c1
victorfreire7 <vhugo.freire159@gmail.com>
2025-12-14 01:02:33 -0300

    validate data

--- src/controllers/Photo.js
+++ src/controllers/Photo.js
@@ -1,27 +1,38 @@
 import multer from 'multer';
 import config from '../config/multer';
 import Photo from '../models/Photo';
+import Aluno from '../models/Aluno';
 
 const upload = multer(config).single('photo');
 
 class Controller {
   store(req, res){
     return upload(req, res, async (e) => {
-      if(e) {
+      if(e || !req.file) {
         return res.status(400).json({ // retorno um bad request
           errors: ['file incompatible'] // verifico se há algum erro (um arquivo nao desejado, por exemplo)
         });
       }
 
+
+
       const { originalname, filename } = req.file;
       const { aluno_id } = req.body;
-      const photo = await Photo.create({
-        originalname,
-        filename,
-        aluno_id
-      })
 
-      return res.json(photo)
+      const aluno = await Aluno.findByPk(aluno_id);
+      if(!aluno){
+        return res.status(400).json({ // retorno um bad request
+          errors: ['user not found'] // verifico se há algum erro (um arquivo nao desejado, por exemplo)
+        });
+      } else {
+        const photo = await Photo.create({
+          originalname,
+          filename,
+          aluno_id
+        })
+
+        return res.json(photo)
+      }
     });
   }
 }
