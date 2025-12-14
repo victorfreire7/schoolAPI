67c2ab8b55eb2ddbf22b979ccce11ec8c192fe26
victorfreire7 <vhugo.freire159@gmail.com>
2025-12-14 00:54:04 -0300

    add photo in DB

--- src/controllers/Photo.js
+++ src/controllers/Photo.js
@@ -1,17 +1,27 @@
 import multer from 'multer';
 import config from '../config/multer';
+import Photo from '../models/Photo';
+
 const upload = multer(config).single('photo');
 
 class Controller {
-  async store(req, res){
-    return upload(req, res, (e) => {
+  store(req, res){
+    return upload(req, res, async (e) => {
       if(e) {
         return res.status(400).json({ // retorno um bad request
           errors: ['file incompatible'] // verifico se há algum erro (um arquivo nao desejado, por exemplo)
         });
       }
 
-      res.json(req.file);
+      const { originalname, filename } = req.file;
+      const { aluno_id } = req.body;
+      const photo = await Photo.create({
+        originalname,
+        filename,
+        aluno_id
+      })
+
+      return res.json(photo)
     });
   }
 }
